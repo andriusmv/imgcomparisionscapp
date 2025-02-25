566f8db2721b733d26f2d7e76d917979abce40b6
Andres Moreno Vasquez <99091918+andriusmv@users.noreply.github.com>
2025-02-25 11:26:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var slide = document.getElementById('slide');
-window.onmousemove = function(e){
-  var x = e.clientX;
-  slide.style.left = x + 'px';
+var container = document.querySelector('.container');
+
+function handleMove(e) {
+    var containerRect = container.getBoundingClientRect();
+    var x = e.clientX - containerRect.left;
+
+    // Calculate width as a percentage of container width
+    var widthPercent = (x / containerRect.width) * 100;
+
+    // Keep width within 0% to 100%
+    if (widthPercent < 0) {
+        widthPercent = 0;
+    } else if (widthPercent > 100) {
+        widthPercent = 100;
+    }
+
+    slide.style.width = widthPercent + '%';
 }
+
+window.onmousemove = handleMove;
+window.ontouchmove = handleMove;
